02559415abc7185d8558af639879ca89c39c9155
Erishen Sun <erishen@qq.com>
2024-07-16 21:02:09 +0800

    update

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,18 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
-};
+  "arrowParens": "always",
+  "bracketSpacing": true,
+  "embeddedLanguageFormatting": "auto",
+  "htmlWhitespaceSensitivity": "css",
+  "insertPragma": false,
+  "jsxSingleQuote": true,
+  "printWidth": 120,
+  "proseWrap": "preserve",
+  "quoteProps": "as-needed",
+  "requirePragma": false,
+  "semi": false,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "trailingComma": "all",
+  "useTabs": false,
+  "vueIndentScriptAndStyle": false
+}
--- Contact.tsx
+++ Contact.tsx
@@ -1,6 +1,96 @@
-import React, {useEffect, useState} from 'react';
-import {View, FlatList, Text, PermissionsAndroid} from 'react-native';
+import React, { useEffect, useState, useMemo } from 'react';
+import { View, FlatList, Text, PermissionsAndroid, StyleSheet, TouchableOpacity, Linking, Alert } from 'react-native';
 import Contacts from 'react-native-contacts';
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10
+  },
+  left: {
+    fontSize: 16,
+    color: 'black',
+  },
+  right: {
+    fontSize: 15,
+    color: 'green'
+  }
+});
+
+const makePhoneCall = (phoneNumber) => {
+  let phoneUrl = `tel:${phoneNumber}`;
+  Linking.canOpenURL(phoneUrl)
+    .then((supported) => {
+      if (!supported) {
+        Alert.alert('Error', `Unable to make a phone call ${phoneNumber}`);
+      } else {
+        return Linking.openURL(phoneUrl);
+      }
+    })
+    .catch((err) => console.error('An error occurred', err));
+};
+
+const ContactItem = ({ item }) => {
+  let { givenName = '', familyName = '', phoneNumbers = [] } = item;
+
+  const newName = useMemo(() => {
+    let name = '';
+
+    if (familyName !== '') {
+      name += familyName + ' ';
+    }
+
+    if (givenName !== '' && givenName !== familyName) {
+      name += givenName;
+    }
+
+    return name;
+  }, [givenName, familyName]);
+
+  const newPhoneNumber = useMemo(() => {
+    let phoneNumber = phoneNumbers?.[0]?.number.toString();
+
+    if (phoneNumber.indexOf(')') !== -1) {
+      const phoneNumberArray = phoneNumber.split(')');
+      phoneNumber = phoneNumberArray?.[1];
+    }
+
+    if (phoneNumber.indexOf(' ') !== -1) {
+      const phoneNumberArray = phoneNumber.split(' ');
+      phoneNumber = phoneNumberArray?.[1];
+    }
+
+    if (phoneNumber.indexOf('+') !== -1) {
+      const phoneNumberArray = phoneNumber.split('+');
+      phoneNumber = phoneNumberArray?.[1];
+    }
+
+    phoneNumber = phoneNumber.trim()
+
+    return phoneNumber;
+  }, [phoneNumbers]);
+
+  const doClickPhoneNumber = () => {
+    makePhoneCall(newPhoneNumber)
+  }
+
+  return (
+    <View style={styles.row}>
+      <Text style={styles.left}>
+        {newName}
+      </Text>
+      <TouchableOpacity onPress={doClickPhoneNumber}>
+        <Text style={styles.right}>
+          {newPhoneNumber}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const Contact = () => {
   const [contacts, setContacts] = useState([]);
@@ -18,13 +108,7 @@ const Contact = () => {
       <FlatList
         data={contacts}
         keyExtractor={item => item.recordID}
-        renderItem={({item}) => (
-          <View>
-            <Text>
-              {item.givenName} {item.familyName} {item.phoneNumbers[0]?.number}
-            </Text>
-          </View>
-        )}
+        renderItem={({ item }) => <ContactItem item={item} />}
       />
     </View>
   );
